e129525e5f04c8148fd0c27042217df4cbe95db0
MoisiAlex <dracken@gmail.com>
2018-05-23 17:26:13 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ window.myCPP = window.myCPP || {};
     function handleContactIncoming(contact) {
         if (contact) {
             logInfoEvent("[contact.onIncoming] Contact is incoming. Contact state is " + contact.getStatus().type);
-            callDiv.innerHTML ='New call from: ' +contact.getActiveInitialConnection().getEndpoint().phoneNumber;
+          
         } else {
             logInfoEvent("[contact.onIncoming] Contact is incoming. Null contact passed to event handler");
         }
